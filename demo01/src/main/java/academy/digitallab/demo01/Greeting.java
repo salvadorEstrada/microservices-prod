60b1684f563bd9b46dbf16574bb94750a4e1bacc
salva <salva@debian-BULLSEYE-live-builder-AMD64>
2023-07-13 23:45:14 -0500

    cambios 1

---
+++ demo01/src/main/java/academy/digitallab/demo01/Greeting.java
@@ -0,0 +1,28 @@
+package academy.digitallab.demo01;
+
+public class Greeting {
+
+    private long id;
+    private String message;
+
+    public Greeting(long id, String message) {
+        this.id = id;
+        this.message = message;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
